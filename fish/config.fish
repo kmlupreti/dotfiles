371aa0c830aacd9e4f876d57b24ae5fbabd9abbe
Kamal <kamalchettri1996@gmail.com>
2023-08-30 16:25:36 +0545

    Format file

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 # aliases
-abbr -a l 'exa'
-abbr -a ls 'exa'
+abbr -a l exa
+abbr -a ls exa
 abbr -a ll 'exa -l'
 abbr -a lt 'exa -T'
 abbr -a pi 'paru -S'
@@ -12,12 +12,12 @@ set PATH $PATH ~/.local/bin
 
 # initial greeting
 function fish_greeting
-	# no greeting
+    # no greeting
 end
 
 # Start GUI at login
 if status --is-login
-  if test (tty) = "/dev/tty1"
-		Hyprland &> /dev/null
-  end
+    if test (tty) = /dev/tty1
+        Hyprland &>/dev/null
+    end
 end
