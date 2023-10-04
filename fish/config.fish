bbb869ac52d34ffa0fec9e4077d06820754e33eb
Kamal <kamalchettri1996@gmail.com>
2023-10-05 01:42:06 +0545

    Change aliases and startup scripts

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,10 @@
 # aliases
-abbr -a l exa
-abbr -a ls exa
-abbr -a ll 'exa -l'
-abbr -a lt 'exa -T'
-abbr -a pi 'paru -S'
-abbr -a pr 'paru -R'
+abbr -a l eza
+abbr -a ls eza
+abbr -a ll 'eza -l'
+abbr -a lt 'eza -T'
+abbr -a pi 'sudo xbps-install'
+abbr -a pr 'sudo xbps-remove'
 
 # append to PATH
 set PATH $PATH ~/.cargo/bin
@@ -18,6 +18,14 @@ end
 # Start GUI at login
 if status --is-login
     if test (tty) = /dev/tty1
-        Hyprland &>/dev/null
+        if test -z "$XDG_RUNTIME_DIR"
+            set -x XDG_RUNTIME_DIR "/tmp/user-$(id -u)"
+            if not test -d "$XDG_RUNTIME_DIR"
+                mkdir -p "$XDG_RUNTIME_DIR"
+                chmod 0700 "$XDG_RUNTIME_DIR"
+            end
+        end
+        river &>/dev/null
     end
 end
+
