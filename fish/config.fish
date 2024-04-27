42a194f62ce221ed5eabb41a6ccdeefade173e3e
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2024-04-27 10:00:31 +0545

    update configs

--- fish/config.fish
+++ fish/config.fish
@@ -3,21 +3,19 @@ abbr -a l eza
 abbr -a ls eza
 abbr -a ll 'eza -l'
 abbr -a lt 'eza -T'
-abbr -a pi 'sudo xbps-install'
-abbr -a pr 'sudo xbps-remove'
+abbr -a pi 'paru -S'
+abbr -a pr 'paru -R'
 abbr -a .. 'cd ..'
 
 # starship prompt
 starship init fish | source
 
-# initial greeting
-function fish_greeting
-    neofetch
-end
+# disable initial greeting
+set fish_greeting
 
 # Start GUI at login
 if status --is-login
     if test (tty) = /dev/tty1
-        river &>/dev/null
+        Hyprland &>/dev/null
     end
 end
