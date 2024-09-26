716e52323c18af23001e0b093a2689679d439af7
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2024-09-26 23:41:33 +0545

    Update configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,36 @@
-# aliases
-abbr -a l eza
-abbr -a ls eza
-abbr -a ll 'eza -l'
-abbr -a lt 'eza -T'
-abbr -a pi 'paru -S'
-abbr -a pr 'paru -R'
-abbr -a .. 'cd ..'
+# set environment variables
+set -x VISUAL helix
+set -x EDITOR helix
 
-# starship prompt
-starship init fish | source
+# configs for interactiive session
+if status is-interactive
+    # aliases
+    abbr -a l eza
+    abbr -a ll 'eza -l'
+    abbr -a lt 'eza -T'
+    abbr -a pi 'paru -S'
+    abbr -a pr 'paru -Rns'
+    abbr -a yz yazi
+    abbr -a hx helix
 
-# disable initial greeting
-set fish_greeting
+    # starship prompt
+    starship init fish | source
 
-# Start GUI at login
-if status --is-login
-    if test (tty) = /dev/tty1
-        Hyprland &>/dev/null
+    # disable initial greeting
+    set fish_greeting
+
+    # ctrl+y to accept suggest autocomplete
+    bind \cy accept-autosuggestion
+
+    # Start GUI at login
+    if status --is-login
+        if test (tty) = /dev/tty1
+            Hyprland &>/dev/null
+        end
+    end
+
+    # auto attach zellij main session
+    if not set -q ZELLIJ
+        zellij attach main || zellij -s main
     end
 end
