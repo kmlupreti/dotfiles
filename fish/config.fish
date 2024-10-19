49afe1a908a375335308d9ed0a85452bfdc0ddcd
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2024-10-19 14:53:24 +0545

    Add yazi file manager function

--- fish/config.fish
+++ fish/config.fish
@@ -28,9 +28,14 @@ if status is-interactive
             Hyprland &>/dev/null
         end
     end
+end
 
-    # auto attach zellij main session
-    if not set -q ZELLIJ
-        zellij attach main || zellij -s main
+# yazi file explorer
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
     end
+    rm -f -- "$tmp"
 end
