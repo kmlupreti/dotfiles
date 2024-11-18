a5e3709f3d3161d23f158a7d68d8fb4aa393fdf9
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2024-11-18 22:08:36 +0545

    Restructure fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,30 +2,30 @@
 set -x VISUAL helix
 set -x EDITOR helix
 
+set -x QT_QPA_PLATFORM "wayland;xcb"
+set -x QT_QPA_PLATFORMTHEME qt6ct
+
+# aliases
+abbr -a l eza
+abbr -a ll 'eza -l'
+abbr -a lt 'eza -T'
+abbr -a pi 'paru -S'
+abbr -a pr 'paru -Rns'
+abbr -a yz yazi
+abbr -a hx helix
+
+# disable initial greeting
+set fish_greeting
+
+# ctrl+y to accept suggest autocomplete
+bind \cy accept-autosuggestion
+
 # configs for interactiive session
 if status is-interactive
-    # aliases
-    abbr -a l eza
-    abbr -a ll 'eza -l'
-    abbr -a lt 'eza -T'
-    abbr -a pi 'paru -S'
-    abbr -a pr 'paru -Rns'
-    abbr -a yz yazi
-    abbr -a hx helix
-
-    # starship prompt
-    starship init fish | source
-
-    # disable initial greeting
-    set fish_greeting
-
-    # ctrl+y to accept suggest autocomplete
-    bind \cy accept-autosuggestion
-
     # Start GUI at login
     if status --is-login
         if test (tty) = /dev/tty1
-            Hyprland &>/dev/null
+            start-cosmic &>/dev/null
         end
     end
 end
