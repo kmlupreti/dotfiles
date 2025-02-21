91ff3d994f73a5cdbe4aa0ffe806bfbd257c25f2
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2025-02-22 01:00:06 +0545

    add zellij setup

--- fish/config.fish
+++ fish/config.fish
@@ -9,22 +9,38 @@ set -x QT_QPA_PLATFORMTHEME qt6ct
 function fish_greeting
     # don't show anyting for now
 end
+
+# aliases
 abbr -a l eza
 abbr -a ll 'eza -l'
+abbr -a la 'eza -la'
 abbr -a lt 'eza -T'
 abbr -a pi 'paru -S'
 abbr -a pr 'paru -Rns'
 
 # ctrl+y to accept suggest autocomplete
-bind ctrl-y accept-autosuggestion
+# bind ctrl-y accept-autosuggestion
+bind \cy accept-autosuggestion
 
 # configs for interactive session
 if status is-interactive
     # Start GUI at login
     if status --is-login
         if test (tty) = /dev/tty1
-            # start-cosmic &>/dev/null
-            Hyprland &>/dev/null
+            start-cosmic &>/dev/null
+        end
+    end
+
+    #setup zellij    
+    if not set -q ZELLIJ
+        if test "$ZELLIJ_AUTO_ATTACH" = true
+            zellij attach -c
+        else
+            zellij
+        end
+
+        if test "$ZELLIJ_AUTO_EXIT" = true
+            kill $fish_pid
         end
     end
 end
