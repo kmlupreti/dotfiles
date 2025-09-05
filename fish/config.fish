bdaebb107fc5bf2f9bc6b6c0c16472bd88871f19
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2025-09-05 10:03:03 +0545

    enable options only in interactive session and add zellij setup

--- fish/config.fish
+++ fish/config.fish
@@ -1,39 +1,38 @@
-# set environment variables
-set -x VISUAL helix
-set -x EDITOR helix
-
 # initial greeting message
 function fish_greeting
     # don't show anyting for now
 end
-
-# aliases
-abbr -a l eza
-abbr -a ll 'eza -l'
-abbr -a la 'eza -la'
-abbr -a lt 'eza -T'
-abbr -a pi 'paru -S'
-abbr -a pr 'paru -Rns'
-
-# ctrl+y to accept suggest autocomplete
-bind ctrl-y accept-autosuggestion
 
 # configs for interactive session
 if status is-interactive
     # Start GUI at login
     if status --is-login
         if test (tty) = /dev/tty1
-            Hyprland &>/dev/null
+            niri &>/dev/null
         end
     end
-end
 
-# yazi file explorer
-function y
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    yazi $argv --cwd-file="$tmp"
-    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-        builtin cd -- "$cwd"
+    # aliases
+    abbr -a l eza
+    abbr -a ll 'eza -l'
+    abbr -a la 'eza -la'
+    abbr -a lt 'eza -T'
+    abbr -a pi 'paru -S'
+    abbr -a pr 'paru -Rns'
+
+    # ctrl+y to accept suggest autocomplete
+    bind ctrl-y accept-autosuggestion
+
+    # yazi setup
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
     end
-    rm -f -- "$tmp"
+
+    # zellij setup
+    eval (zellij setup --generate-auto-start fish | string collect)
 end
