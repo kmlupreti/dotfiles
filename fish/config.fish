8824a5bf5cc4714a92df2408061a07ece0598b8f
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-06-09 20:17:11 +0545

    add back option to start gui at login at tty1 and remove yazi function

--- fish/config.fish
+++ fish/config.fish
@@ -1,28 +1,22 @@
-# initial greeting message
 function fish_greeting
-    # don't show anyting for now
+    # don't show greeting
 end
 
 # configs for interactive session
 if status is-interactive
     # aliases
-    abbr -a l eza
+    abbr -a l 'eza --icons=always'
     abbr -a ll 'eza -l'
-    abbr -a la 'eza -la'
-    abbr -a lt 'eza -T'
     abbr -a pi 'paru -S'
     abbr -a pr 'paru -Rns'
 
     # ctrl+y to accept suggest autocomplete
     bind ctrl-y accept-autosuggestion
 
-    # yazi setup
-    function y
-        set tmp (mktemp -t "yazi-cwd.XXXXXX")
-        yazi $argv --cwd-file="$tmp"
-        if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-            builtin cd -- "$cwd"
+    # Start GUI at login
+    if status --is-login
+        if test (tty) = /dev/tty1
+            niri &>/dev/null
         end
-        rm -f -- "$tmp"
     end
 end
