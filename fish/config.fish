9ef69a03fedf9e34d870988ac4109093426ea78f
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-06-18 20:30:15 +0545

    simplify fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,22 +1,4 @@
-function fish_greeting
-    # don't show greeting
-end
-
-# configs for interactive session
-if status is-interactive
-    # aliases
-    abbr -a l 'eza --icons=always'
-    abbr -a ll 'eza -l'
-    abbr -a pi 'paru -S'
-    abbr -a pr 'paru -Rns'
-
-    # ctrl+y to accept suggest autocomplete
-    bind ctrl-y accept-autosuggestion
-
-    # Start GUI at login
-    if status --is-login
-        if test (tty) = /dev/tty1
-            niri &>/dev/null
-        end
-    end
+# run compositor in login shell
+if status --is-login
+    niri &>/dev/null
 end
